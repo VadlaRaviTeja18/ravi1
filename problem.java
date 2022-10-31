bf487f2106258d4721651f5df32d3d50ebef33c7
VadlaRaviTeja18 <117094686+VadlaRaviTeja18@users.noreply.github.com>
2022-10-31 19:48:21 +0530

    Add files via upload

---
+++ problem.java
@@ -0,0 +1,10 @@
+public class problem {
+    public static void main(String[] args) {
+        int[] A={1,2,3,4,5,6,7,8,9,10};
+        int sum=0;
+        for(int i=0;i<A.length;i++){
+            sum+=A[i];
+        }
+        System.out.println((float)sum/A.length);
+    }
+}
